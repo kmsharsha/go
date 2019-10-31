daf3f21cc052e9c5a709b78add447fba9de4294c
Sri Harsha <sriharsha@Sris-MacBook-Air.local>
2019-10-31 15:06:39 +0530

    initial commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+echo "Downloading go packages..."
+wget https://dl.google.com/go/go1.11.4.linux-amd64.tar.gz
+
+echo "Unpacking go packages..."
+sudo tar -C /usr/local -xzvf go1.11.4.linux-amd64.tar.gz
+
+echo "Updating go path..."
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc
+source ~/.bashrc
+
+echo "Removing downloaded tar file..."
+rm go1.11.4.linux-amd64.tar.gz
+
+echo "Checking go version..."
+go version
